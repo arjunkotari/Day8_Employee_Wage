894f3701af0f1d91edf907feb69e1cd341827f8e
arjunkotari <arjun.kotari123@gmail.com>
2021-07-19 23:54:49 +0530

    Added:Calcuting daily emp wage

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -3,18 +3,24 @@ package employeeWage;
 import java.util.Random;
 
 public class EmployeeWage {
+	private static int IS_FULL_TIME=1;
+	private static int EMP_RATE_PER_HOUR=20;	
 	/*
 	 * Calculating Employee Wage Computation 
 	 */
 	public static void employeeWage() {
 		//Displaying welcome message
-		System.out.println("Welcome to Employee Wage Computation");
-		int check = 1;
+		System.out.println("Welcome to Employee Wage Computation");		
+		int empHrs=0;
+		int empWage=0;
+		//Calculating daily employee wage
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck == check)
-			System.out.println("Employee is Present");
+		if (empCheck == IS_FULL_TIME)
+			empHrs=8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs=0;
+		empWage	= empHrs * EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: " +empWage);
 		
 	}
 	public static void main(String[] args) {

5eea34bf839f1eac9878ae4be143bdea561e2ba4
arjunkotari <arjun.kotari123@gmail.com>
2021-07-20 00:23:59 +0530

    Added:Total wage added

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -7,16 +7,18 @@ public class EmployeeWage {
 	private static final int IS_PART_TIME = 2;
 	private static int EMP_RATE_PER_HOUR=20;
 	private static final int NUM_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MNTH = 10;
 	/*
 	 * Calculating Employee Wage Computation 
 	 */
 	public static void employeeWage() {
-		System.out.println("Welcome to Employee Daily Wage");
+System.out.println("Welcome to Employee Daily Wage");
 		
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage = 0;
-		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while (totalEmpHrs <= MAX_HRS_IN_MNTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 		int empCheck =(int) Math.floor(Math.random()*10) % 3;
 		switch (empCheck) {
 		case IS_FULL_TIME:
@@ -28,10 +30,10 @@ public class EmployeeWage {
 		default:
 			empHrs = 0;
 		}
-		empWage = empHrs * EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage;
-		System.out.println("Emp Wage: " + empWage);
+		totalEmpHrs += empHrs;
+		System.out.println("Day# " + totalWorkingDays + "Emp Hr: " +empHrs);
 		}
+		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 

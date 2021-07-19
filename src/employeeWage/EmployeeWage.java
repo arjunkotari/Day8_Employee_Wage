f4b59deab284afd3554a3e75081cb08716b90b47
arjunkotari <arjun.kotari123@gmail.com>
2021-07-19 23:35:21 +0530

    Added:welcome message of Employee Wage Computation

---
+++ src/employeeWage/EmployeeWage.java
@@ -0,0 +1,15 @@
+package employeeWage;
+
+public class EmployeeWage {
+	/*
+	 * Calculating Employee Wage Computation 
+	 */
+	public static void employeeWage() {
+		//Displaying welcome message
+				System.out.println("Welcome to Employee Wage Computation");
+	}
+	public static void main(String[] args) {
+		employeeWage();
+	}
+
+}

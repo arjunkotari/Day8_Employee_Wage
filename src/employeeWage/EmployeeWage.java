76567fff481cd309a7ebb1021c94890f773c4420
arjunkotari <arjun.kotari123@gmail.com>
2021-07-20 00:27:37 +0530

    Added: refactor code

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -3,21 +3,20 @@ package employeeWage;
 import java.util.Random;
 
 public class EmployeeWage {
-	public static final int IS_FULL_TIME = 1;
-	private static final int IS_PART_TIME = 2;
-	private static int EMP_RATE_PER_HOUR=20;
-	private static final int NUM_OF_WORKING_DAYS = 2;
-	public static final int MAX_HRS_IN_MNTH = 10;
+	//Constants
+		public static final int IS_PART_TIME = 1;
+		public static final int IS_FULL_TIME = 2;
 	/*
 	 * Calculating Employee Wage Computation 
 	 */
-	public static void employeeWage() {
-System.out.println("Welcome to Employee Daily Wage");
-		
+	public static int employeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+		System.out.println("Welcome to Employee Daily Wage");
+		//Variables
 		int empHrs = 0;
 		int totalEmpHrs = 0;
 		int totalWorkingDays = 0;
-		while (totalEmpHrs <= MAX_HRS_IN_MNTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+		//Computation
+		while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
 			totalWorkingDays++;
 		int empCheck =(int) Math.floor(Math.random()*10) % 3;
 		switch (empCheck) {
@@ -31,15 +30,14 @@ System.out.println("Welcome to Employee Daily Wage");
 			empHrs = 0;
 		}
 		totalEmpHrs += empHrs;
-		System.out.println("Day# " + totalWorkingDays + "Emp Hr: " +empHrs);
+		System.out.println("Day#: " + totalWorkingDays + "Emp Hr: " +empHrs);
 		}
-		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
-	}
-
-
-	public static void main(String[] args) {
-		employeeWage();
-	}
-
+		return totalEmpWage;
+		}
+		public static void main(String[] args) {
+			employeeWage("DMart", 20, 2, 10);
+			employeeWage("Reliance", 10, 4, 20);
+		}
 }

9857a1e7510720cd17390848852d8edef9007fe1
arjunkotari <arjun.kotari123@gmail.com>
2021-07-20 00:20:47 +0530

    Added:Calculation Wages for 20 days

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -6,15 +6,18 @@ public class EmployeeWage {
 	public static final int IS_FULL_TIME = 1;
 	private static final int IS_PART_TIME = 2;
 	private static int EMP_RATE_PER_HOUR=20;
+	private static final int NUM_OF_WORKING_DAYS = 2;
 	/*
 	 * Calculating Employee Wage Computation 
 	 */
 	public static void employeeWage() {
-		System.out.println("Welcome to Employee Daily Wage");		
+		System.out.println("Welcome to Employee Daily Wage");
+		
 		int empHrs = 0;
 		int empWage = 0;
-		int empCheck =(int) Math.floor(Math.random()*10)%3;
-		
+		int totalEmpWage = 0;
+		for (int day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+		int empCheck =(int) Math.floor(Math.random()*10) % 3;
 		switch (empCheck) {
 		case IS_FULL_TIME:
 			empHrs = 8;
@@ -26,8 +29,12 @@ public class EmployeeWage {
 			empHrs = 0;
 		}
 		empWage = empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage:" + empWage);
+		totalEmpWage += empWage;
+		System.out.println("Emp Wage: " + empWage);
+		}
+		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
+
 
 	public static void main(String[] args) {
 		employeeWage();

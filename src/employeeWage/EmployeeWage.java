4008a182e4ce3df2c46b5211620678f48b8c021e
arjunkotari <arjun.kotari123@gmail.com>
2021-07-20 00:00:39 +0530

    Added:Part time calculation and full time calculation

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -4,23 +4,24 @@ import java.util.Random;
 
 public class EmployeeWage {
 	private static int IS_FULL_TIME=1;
-	private static int EMP_RATE_PER_HOUR=20;	
+	private static int EMP_RATE_PER_HOUR=20;
+	private static int IS_PART_TIME = 2;
 	/*
 	 * Calculating Employee Wage Computation 
 	 */
 	public static void employeeWage() {
-		//Displaying welcome message
-		System.out.println("Welcome to Employee Wage Computation");		
-		int empHrs=0;
-		int empWage=0;
-		//Calculating daily employee wage
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if (empCheck == IS_FULL_TIME)
-			empHrs=8;
-		else
-			empHrs=0;
-		empWage	= empHrs * EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " +empWage);
+			System.out.println("Welcome to Employee Daily Wage");		
+			int empHrs = 0;
+			int empWage = 0;
+			double empCheck = Math.floor(Math.random()*10)%3;
+			if(empCheck == IS_FULL_TIME)
+				empHrs = 8;
+			else if (empCheck == IS_PART_TIME)
+				empHrs = 4;
+			else
+				empHrs = 0;
+			empWage = empHrs * EMP_RATE_PER_HOUR;
+			System.out.println("Emp Wage:" + empWage);
 		
 	}
 	public static void main(String[] args) {

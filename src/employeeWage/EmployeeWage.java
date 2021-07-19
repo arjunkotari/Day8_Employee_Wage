b0abe2e30d08c4ed58d82642f6004768866a063d
arjunkotari <arjun.kotari123@gmail.com>
2021-07-19 23:45:03 +0530

    Added:Employee attendence

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -1,12 +1,21 @@
 package employeeWage;
 
+import java.util.Random;
+
 public class EmployeeWage {
 	/*
 	 * Calculating Employee Wage Computation 
 	 */
 	public static void employeeWage() {
 		//Displaying welcome message
-				System.out.println("Welcome to Employee Wage Computation");
+		System.out.println("Welcome to Employee Wage Computation");
+		int check = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if(empCheck == check)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+		
 	}
 	public static void main(String[] args) {
 		employeeWage();

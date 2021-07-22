a457ba985a802211fb275f9976e05a85266def31
arjunkotari <arjun.kotari123@gmail.com>
2021-07-22 18:29:39 +0530

    Added:array to store multiple companies employee wages

--- src/employeeWage/EmployeeWage.java
+++ src/employeeWage/EmployeeWage.java
@@ -2,13 +2,13 @@ package employeeWage;
 
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class EmployeeWage {
 	// Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-	public static int company1;
-	public static int company2;
+	public static int company;
 	public static int empHrs;
 	public static int totalEmpHrs;
 	public static int totalWorkingDays;
@@ -19,7 +19,6 @@ public class EmployeeWage {
 	public static int employeeWageBuilder(String company, int empRatePerHour, int numOfWorkingDays,
 			int maxHoursPerMonth) {
 		System.out.println("Welcome to Employee Daily Wage " + company);
-		// Variables
 		empHrs = 0;
 		totalEmpHrs = 0;
 		totalWorkingDays = 0;
@@ -46,8 +45,26 @@ public class EmployeeWage {
 	}
 
 	public static void main(String[] args) {
-		// save the total wage for each company
-		company1 = employeeWageBuilder("DMart", 20, 2, 10);
-		company2 = employeeWageBuilder("Reliance", 10, 4, 20);
+		// saving the total wage for each company in Array.
+		// company1 = employeeWageBuilder("DMart", 20, 2, 10);
+		// company2 = employeeWageBuilder("Reliance", 10, 4, 20);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter num of companies you want to add");
+		int num = scanner.nextInt();
+		// array
+		int[] array = new int[num];
+		for (int i = 0; i < num; i++) {
+			System.out.println("Enter Comapny Name");
+			String name = scanner.next();
+			System.out.println("Enter Emp Rate PerHour");
+			int empRatePerHour = scanner.nextInt();
+			System.out.println("Enter numOf Working Days");
+			int numOfWorkingDays = scanner.nextInt();
+			System.out.println("Enter max Hours Per Month ");
+			int maxHoursPerMonth = scanner.nextInt();
+			company = employeeWageBuilder(name, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+			array[i] = company;
+		}
+		scanner.close();
 	}
 }
